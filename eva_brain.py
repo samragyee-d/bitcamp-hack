e27001551e2e6019ad7c1cb8cd4f8aa20f6af3b3
Juhi <juhic3162@gmail.com>
2025-04-12 05:24:06 -0400

    getting new changes to work

--- eva_brain.py
+++ eva_brain.py
@@ -1,5 +1,10 @@
-#prompting gemini code goes in this file
+#prompting gemini code goes in this file, pretty much logic goes here
 
 from google.generativeai import GenerativeModel
 
 model = GenerativeModel("gemini-pro")
+
+#generating messages to send back
+# if angry for more than 10 minutes, what is the response
+
+# generate weekly summary email recap (as time permits)
